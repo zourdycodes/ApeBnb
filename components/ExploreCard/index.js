7c47357e0877403b3009d52bf4f2e51f1903f6f8
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-07 14:14:43 +0700

    start up to build explore card

---
+++ components/ExploreCard/index.js
@@ -0,0 +1,7 @@
+export const ExploreCard = ({ location, image, distance }) => {
+  return (
+    <div>
+      <h1>Hello Explore</h1>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
+import { useState } from "react";
 
-import { Header, Banner } from "../components";
+import { Header, Banner, ExploreCard } from "../components";
 
-export default function Home() {
+export default function Home({ exploreData }) {
+  // const [data, setData] = useState(exploreData);
+
   return (
     <div className="">
       <Head>
@@ -14,6 +17,37 @@ export default function Home() {
       <Header />
       {/* BANNER */}
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore nearby.</h2>
+
+          {/* fetch data from server */}
+          {exploreData?.map((data, index) => {
+            const { img: image, location, distance } = data;
+
+            return (
+              <ExploreCard
+                key={index}
+                image={image}
+                location={location}
+                distance={distance}
+              />
+            );
+          })}
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("https://links.papareact.com/pyp");
+  const exploreData = await response.json();
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
