3326be004ff9f546e710a959b42de7a2de6ab317
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-07 09:42:37 +0700

    finishing and polishing header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  MenuIcon,
+  GlobeAltIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
 
 export const Header = () => {
   return (
@@ -19,13 +25,21 @@ export const Header = () => {
         <input
           type="text"
           placeholder="Search your lovable spot!"
-          className=" flex-grow pl-5 bg-transparent outline-none"
+          className=" flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
 
       {/* RIGHT */}
-      <div></div>
+      <div className="flex items-center space-x-4 justify-end">
+        <p className="hidden md:inline">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6 cursor-pointer" />
+          <UserCircleIcon className="h-6 cursor-pointer" />
+        </div>
+      </div>
     </header>
   );
 };
