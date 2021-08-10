1d3cce89f5a105758ab9aa457644eec54c2d4e68
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-10 15:21:09 +0700

    finish the build

--- components/MapGL/index.js
+++ components/MapGL/index.js
@@ -1,20 +1,26 @@
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { useState } from "react";
 import { getCenter } from "geolib";
 
 export const MapComponent = ({ searchResults }) => {
-  const [viewport, setViewport] = useState({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
-    width: "100%",
-    height: "100%",
-  });
-
+  const [selectedLocation, setSelectedLocation] = useState({});
+  // transform the search results data into the
+  // { longitude: bla bla bla, latitude: bla bla bla }
   const coordinates = searchResults?.map((result) => ({
     longitude: result.long,
     latitude: result.lat,
   }));
+
+  // The Latitude and Longitude of the center of locations coordinates
+  const centerCoords = getCenter(coordinates);
+
+  const [viewport, setViewport] = useState({
+    latitude: centerCoords.latitude,
+    longitude: centerCoords.longitude,
+    zoom: 10,
+    width: "100%",
+    height: "100%",
+  });
 
   return (
     <ReactMapGL
@@ -22,6 +28,38 @@ export const MapComponent = ({ searchResults }) => {
       mapStyle="mapbox://styles/zourdycodes/cks5j870sd5l917pla4ceyol8"
       mapboxApiAccessToken={process.env.mapbox_key}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    ></ReactMapGL>
+    >
+      {searchResults?.map((result, index) => (
+        <div key={index}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              role="img"
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+              aria-label="push-pin"
+            >
+              📌
+            </p>
+          </Marker>
+
+          {/* POPUP */}
+          {selectedLocation.long === result.long ? (
+            <Popup
+              closeOnClick={true}
+              onClose={() => setSelectedLocation({})}
+              longitude={result.long}
+              latitude={result.lat}
+            >
+              <div>{result.title}</div>
+            </Popup>
+          ) : null}
+        </div>
+      ))}
+    </ReactMapGL>
   );
 };
